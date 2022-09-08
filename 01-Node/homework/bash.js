339b013fab71a77d0c65a3c975db0db425d4549e
Leomez <leoariel17@gmail.com>
2022-09-08 15:59:37 -0300

    01 hasta tail

--- 01-Node/homework/bash.js
+++ 01-Node/homework/bash.js
@@ -1,7 +1,7 @@
 const { pwd } = require('./commands/index');
 const commands = require('./commands/index');
 
-const done = function(data) {
+const done = function (data) {
     process.stdout.write(data);
     process.stdout.write('\nprompt > ');
 }
@@ -10,21 +10,22 @@ const done = function(data) {
 process.stdout.write('prompt > ');
 // El evento stdin 'data' se dispara cuando el user escribe una línea
 process.stdin.on('data', function (data) {
-  var cmd = data.toString().trim(); // remueve la nueva línea
-
+    var entrada = data.toString().trim().split(' '); // remueve la nueva línea
+    let cmd = entrada.shift()
+    //   if(input[0]==='echo')
     if (commands[cmd]) {
-        commands[cmd](done)
+        commands[cmd](entrada,done)
     }
-
     else {
         done('comando inexistete')
     }
-//   if(cmd === 'date') {
-//     commands.date(done)
-//   }
-//   if(cmd === 'pwd') {
-//    commands.pwd(done)    
-//   }
-  
-//   process.stdout.write('\nprompt > ');
+
+    //   if(cmd === 'date') {
+    //     commands.date(done)
+    //   }
+    //   if(cmd === 'pwd') {
+    //    commands.pwd(done)    
+    //   }
+
+    //   process.stdout.write('\nprompt > ');
 });
--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,29 +1,62 @@
-const fs =  require('fs')
+const fs = require('fs')
 
 
 module.exports = {
-    pwd: (data) => {
-        data(__dirname)
+    pwd: (done, input) => {
+        done(__dirname)
         // process.stdout.write(process.cwd()) es lo mismo <--------
         // process.stdout.write(process.mainModule.path) es lo mismo  <--------            
-    },             
-    date: (data) => {
-        data(Date())
     },
-    ls: (data) => {
-        fs.readdir('.', (err,files) => {
-            if(err) throw err;
-            files.forEach((file) => 
-                data(file.toString())
+    date: (done, input) => {
+        done(Date())
+    },
+    ls: (done, input) => {
+        fs.readdir('.', (err, files) => {
+            if (err) throw err;
+            files.forEach((file) =>
+                done(file.toString())
             )
         }
-    )},
-    echo: (data) => {
-        data(
-            console.log('hola')
+        )
+    },
+    // echo: (done, input) => {
+    //     done(input)
+    // }
+    echo: function (input, done) {
+        done(input.join(' '))
+    },
+    cat: (input, done) => {
+        fs.readFile(`./${input}`, (err, file) => {
+            if (err) throw err;
+            else done(`\n***********************************************************************
+                        ${file.toString()}
+                    \n\n***********************************************************************`
+            )
+
+        })
+    },
+    head: (input, done) => {
+        fs.readFile(`./${input}`, (err, file) => {
+            if (err) throw err;
+            let lines = file.toString().split('\n').slice(0, 10);
+            done(`\n\n***********************************************************************
+                    ${lines.join('\n')}
+                \n\n************************************************************************`
+            )
+
+        })
+    },
+    tail: (input, done) => {
+        fs.readFile(`./${input}`, (err, file) => {
+            if (err) throw err;
+            let lines = file.toString().split('\n').slice(-10);
+            done(`\n\n***********************************************************************
+                    ${lines.join('\n')}
+                \n\n************************************************************************`
+            )
+        }
         )
     }
-    
 }
 
 
