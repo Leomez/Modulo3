fab864da767a783496284554bbbaf223184657ac
Leomez <leoariel17@gmail.com>
2022-09-07 22:05:18 -0300

    v1.0

--- 01-Node/homework/commands/index.js
+++ 01-Node/homework/commands/index.js
@@ -1,3 +1,4 @@
+const fs =  require('fs')
 
 
 module.exports = {
@@ -5,10 +6,18 @@ module.exports = {
         data(__dirname)
         // process.stdout.write(process.cwd()) es lo mismo <--------
         // process.stdout.write(process.mainModule.path) es lo mismo  <--------            
-    },    
-            
+    },             
     date: (data) => {
         data(Date())
-    }            
+    },
+    ls: (data) => {
+        fs.readdir('.', (err,files) => {
+            if(err) throw err;
+            files.forEach((file) => 
+                data(file.toString())
+            )
+        }
+    )},             
 }
+
 
