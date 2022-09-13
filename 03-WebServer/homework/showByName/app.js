d3a07dbc39fbd1919472d360dd99b7910f76b3e9
Leomez <leoariel17@gmail.com>
2022-09-13 15:58:36 -0300

    server v1

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,4 +1,21 @@
 var fs  = require("fs")
 var http  = require("http")
+const images = ['arcoiris_doge','badboy_doge','code_doge','resaca_doge','retrato_doge','sexy_doge']
 
 // Escribí acá tu servidor
+http.createServer( (req, res) => {
+    res.writeHead(200, {'conten-type' : 'image/jpeg'})
+    switch (req.url) {
+        case `/${images[1]}`:
+            let image = fs.readFileSync(`${__dirname}/images/${images[1]}.jpg`)
+            res.end(image)  
+            break;
+    
+        default:
+            break;
+    }
+    
+    
+    console.log('server runing...');
+    // console.log(req);
+}).listen(3001, 'localhost')
