7c01b78516b0454c6e9b0de0fbec81060133ae86
Leomez <leoariel17@gmail.com>
2022-09-14 16:05:42 -0300

    Beatles

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -1,21 +1,59 @@
-var fs  = require("fs")
-var http  = require("http")
-const images = ['arcoiris_doge','badboy_doge','code_doge','resaca_doge','retrato_doge','sexy_doge']
+var fs = require("fs")
+var http = require("http")
+const images = ['arcoiris_doge', 'badboy_doge', 'code_doge', 'resaca_doge', 'retrato_doge', 'sexy_doge']
 
 // Escribí acá tu servidor
-http.createServer( (req, res) => {
-    res.writeHead(200, {'conten-type' : 'image/jpeg'})
+http.createServer((req, res) => {
     switch (req.url) {
+        case `/${images[0]}`:
+            {
+                res.writeHead(200, { 'conten-type': 'image/jpeg' })
+                let image = fs.readFileSync(`${__dirname}/images/${images[0]}.jpg`)
+                res.end(image)
+                break
+            }
         case `/${images[1]}`:
-            let image = fs.readFileSync(`${__dirname}/images/${images[1]}.jpg`)
-            res.end(image)  
-            break;
-    
+            {
+                res.writeHead(200, { 'conten-type': 'image/jpeg' })
+                let image = fs.readFileSync(`${__dirname}/images/${images[1]}.jpg`)
+                res.end(image)
+                break
+            }
+        case `/${images[2]}`:
+            {
+                res.writeHead(200, { 'conten-type': 'image/jpeg' })
+                let image = fs.readFileSync(`${__dirname}/images/${images[2]}.jpg`)
+                res.end(image)
+                break
+            }
+        case `/${images[3]}`:
+            {
+                res.writeHead(200, { 'conten-type': 'image/jpeg' })
+                let image = fs.readFileSync(`${__dirname}/images/${images[3]}.jpg`)
+                res.end(image)
+                break
+            }
+        case `/${images[4]}`:
+            {
+                res.writeHead(200, { 'conten-type': 'image/jpeg' })
+                let image = fs.readFileSync(`${__dirname}/images/${images[4]}.jpg`)
+                res.end(image)
+                break
+            }
+        case `/${images[5]}`:
+            {
+                res.writeHead(200, { 'conten-type': 'image/jpeg' })
+                let image = fs.readFileSync(`${__dirname}/images/${images[5]}.jpg`)
+                res.end(image)
+                break
+            }
         default:
-            break;
-    }
-    
-    
-    console.log('server runing...');
-    // console.log(req);
-}).listen(3001, 'localhost')
+            {
+                res.writeHead(404, { 'conten-type': 'text/text' })
+                res.end('Page not found')
+                break
+            }
+        }
+        console.log('server runing...');
+    }).listen(3001, 'localhost')
+
