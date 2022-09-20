54aa799692ea1801579b262fbd4f517b85dece06
Leomez <leoariel17@gmail.com>
2022-09-20 14:46:23 -0300

    capitulo 2 completo

--- 04-AdvancePromises/homework/src/pledge.js
+++ 04-AdvancePromises/homework/src/pledge.js
@@ -6,26 +6,44 @@ Promises Workshop: construye la libreria de ES6 promises, pledge.js
 function $Promise(executor) {
     if (typeof executor !== 'function') throw new TypeError('executor function')
     this._state = 'pending'
+    this._handlerGroups = [];
     executor(this._internalResolve.bind(this), this._internalReject.bind(this) )
 
 }
 $Promise.prototype._internalResolve = function (value) {
     if (this._state === 'pending') {
         this._state = 'fulfilled'
-        this._value = value        
-    }
-    // return $Promise(this._value)
-   
+        this._value = value
+        this._callHandlers()      //invoca al callHandleres
+    }    
 }
+
 
 $Promise.prototype._internalReject = function (value) { 
     if (this._state === 'pending') {
         this._state = 'rejected'
         this._value = value
-        
+        this._callHandlers() //invoca al callHandleres
     }
-    // return $Promise(this._value)
+}
 
+$Promise.prototype.then = function (successCb, errorCb) {
+    if (typeof successCb !== 'function') successCb = false;
+    if (typeof errorCb !== 'function') errorCb=false
+    this._handlerGroups.push({successCb, errorCb})
+    if (this._state !== 'pending') this._callHandlers()
+}
+
+$Promise.prototype._callHandlers = function () {              //cuando es invocada
+ while (this._handlerGroups.length > 0) {                     //se fija si hay handlers en el handlerGroups
+    let current = this._handlerGroups.shift()                 //si hay, tomo el primero del arreglo y lo guarda en current
+    if (this._state === 'fulfilled') {                        //comprueva si es fulfilled o rejected
+        current.successCb && current.successCb(this._value)   //si es fulfilled le pasa el value correspondiente
+    }
+    else if (this._state === 'rejected') {                    //si es rejected, le pasa el value para la razon de rechazo 
+        current.errorCb && current.errorCb(this._value)
+    }
+ }   
 }
 
 module.exports = $Promise;
