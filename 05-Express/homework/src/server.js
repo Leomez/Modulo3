cfb5de31f44441d8be55c71751a9b3faac30363d
Leomez <leoariel17@gmail.com>
2022-09-23 15:38:32 -0300

    primer ruta

--- 05-Express/homework/src/server.js
+++ 05-Express/homework/src/server.js
@@ -2,16 +2,38 @@
 const express = require("express");
 
 const STATUS_USER_ERROR = 422;
-
+let id = 1;
 // This array of posts persists in memory across requests. Feel free
 // to change this to a let binding if you need to reassign it.
 const posts = [];
 
 const server = express();
 // to enable parsing of json bodies for post requests
-// server.use(express.json());
+server.use(express.json());
 
 // TODO: your code to handle requests
+const PATH = '/posts'
+server.post(PATH, (req, res) => {
+    const { author, title, contents } = req.body;
+    console.log(author, title, contents);
+    if (!author || !title || !contents) {
+        return res.status(STATUS_USER_ERROR).json(
+            {
+                error: "No se recibieron los parámetros necesarios para crear el Post"
+            });
+    }
+    // res.send('done')
+    const post = {
+        author,
+        title,
+        contents,
+        id: id++
+    }
+    posts.push(post)
+    res.status(200).json(post)
 
+
+
+})
 
 module.exports = { posts, server };
